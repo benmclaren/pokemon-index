9aef3299c086b9e8adb0aab8f85cf5a6e15972cd
Ben McLaren <benmclaren@live.co.uk>
2024-05-21 19:40:14 +0100

    data comitted

--- scripts/enrich-data.js
+++ scripts/enrich-data.js
@@ -1,56 +1,76 @@
-import algoliasearch from "algoliasearch"
-import * as dotenv from "dotenv"
+import algoliasearch from "algoliasearch";
+import * as dotenv from "dotenv";
 
 dotenv.config({
   path: ".env.local",
-})
+});
 
 const client = algoliasearch(
-  process.env.NEXT_PUBLIC_ALGOLIA_ID, 
+  process.env.NEXT_PUBLIC_ALGOLIA_ID,
   process.env.ALGOLIA_ADMIN
-)
+);
 
-const algoliaIndex = client.initIndex("pokemon")
+const algoliaIndex = client.initIndex("pokemon");
 
-const fetchPokemonData = async (id) => {
+const fetchPokemonData = async (hit) => {
   try {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    const pokemonData = await response.json()
-    console.log(pokemonData)
-    const imageUrl = pokemonData.sprites.other['official-artwork'].front_default
-    const gameVersions = pokemonData.game_indicies
-    // console.log(typeof gameVersions)
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${hit.id}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const pokemonData = await response.json();
+    const imageUrl = pokemonData.sprites.other['official-artwork'].front_default;
+    const gameVersions = pokemonData.game_indices.map((game) => game.version.name);
+
+    return {
+      ...hit,
+      objectID: hit.objectID,
+      imageUrl,
+      gameVersions
+    };
   } catch (err) {
-    console.log(err)
+    
   }
 };
 
 const fetchAlgoliaData = async () => {
-  const algoliaHits = [];
+  let algoliaHits = [];
   await algoliaIndex.browseObjects({
     query: '',
-    batch: batch => {
-      batch.forEach(hit => {
-        algoliaHits.push(hit)
-        // console.log(hit.id)
-        const algoliaPokemonId = hit.id
-        fetchPokemonData(algoliaPokemonId)
-      })
-    }
-  })
-  return algoliaHits
+    batch: (batch) => {
+      algoliaHits = algoliaHits.concat(batch);
+    },
+  });
+
+  const updatedRecordsPromises = algoliaHits.map(async (hit) => {
+    const updatedRecord = await fetchPokemonData(hit);
+    return updatedRecord;
+  });
+
+  const updatedRecords = await Promise.all(updatedRecordsPromises);
+  return updatedRecords.filter(hit => hit);
 };
 
 const combinedData = async () => {
-  // const pokemonData = fetchPokemonData()
-  const algoliaData = fetchAlgoliaData()  
-}
+  try {
+    const algoliaData = await fetchAlgoliaData();
+
+    if (algoliaData.length > 0) {
+      await algoliaIndex.partialUpdateObjects(algoliaData)
+        .then((response) => {
+          // console.log("Update response:", response);
+          // console.log("Algolia data successfully updated!");
+        })
+        .catch((err) => {
+          // console.error("Error during Algolia update:", err);
+        });
+    } else {
+      // console.log("No data to update.");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 
-
-
-// combinedData()
-fetchAlgoliaData()
-// fetchPokemon()
-
-
+combinedData()
